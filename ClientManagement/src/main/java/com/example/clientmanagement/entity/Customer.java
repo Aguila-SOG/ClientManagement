1ab098a7fda0518220f7350c2ebf57a558b48a56
Aguila-SOG <serorogav@alu.edu.gva.es>
2026-05-15 12:51:42 +0200

    Customer

---
+++ ClientManagement/src/main/java/com/example/clientmanagement/entity/Customer.java
@@ -0,0 +1,64 @@
+package com.example.clientmanagement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String nick;
+
+    @Column(nullable = false, length = 40)
+    private String platform;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
