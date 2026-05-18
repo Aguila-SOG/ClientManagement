4401b1e092fb69cb2e400ba8420e75c93a5fd92c
Aguila-SOG <serorogav@alu.edu.gva.es>
2026-05-18 19:23:17 +0200

    Encontrar, modificar y delete Custumers

--- ClientManagement/src/main/java/com/example/clientmanagement/repository/CustomerDAO.java
+++ ClientManagement/src/main/java/com/example/clientmanagement/repository/CustomerDAO.java
@@ -62,8 +62,64 @@ public class CustomerDAO {
         }
     }
 
-//    public Customer findCustomer(Long id){
-//        String query = "SELECT id, nick, platform, name, email FROM customer WHERE id = ?";
-//
-//    }
+    public Customer findCustomer(Long id){
+        Customer customer = null;
+        String query = "SELECT id, nick, platform, name, email FROM customer WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery(query);
+            while(resultSet.next()){
+                 customer = new Customer(
+                        resultSet.getLong("id"),
+                        resultSet.getString("nick"),
+                        resultSet.getString("platform"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customer;
+    }
+
+    public Customer editCustomer(Long id, String nick, String platform, String name, String email){
+        Customer customer = null;
+        String query = "UPDATE customer SET nick = ?, platform = ?, name = ?, email = ? WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, nick);
+            preparedStatement.setString(2, platform);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, email);
+            preparedStatement.setLong(5, id);
+            int updated = preparedStatement.executeUpdate();
+            if (updated != 1) {
+                throw new SQLException("Expected 1 row inserted, got " + updated);
+            }
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    customer.setId(resultSet.getLong(1));
+                }
+            }
+            return customer;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error edit customer", e);
+        }
+    }
+
+    public void deleteCustomer(Long id){
+        String query = "DELETE FROM customer WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setLong(1, id);
+            int updated = preparedStatement.executeUpdate();
+            if (updated != 1) {
+                throw new SQLException("Expected 1 row inserted, got " + updated);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error delete customer", e);
+        }
+    }
 }
