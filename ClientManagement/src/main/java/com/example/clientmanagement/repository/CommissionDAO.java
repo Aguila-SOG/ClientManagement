2aeed5aa16746eb2f30550ee83140fd40b3cac56
Aguila-SOG <serorogav@alu.edu.gva.es>
2026-05-22 18:24:11 +0200

    fix error comision

--- ClientManagement/src/main/java/com/example/clientmanagement/repository/CommissionDAO.java
+++ ClientManagement/src/main/java/com/example/clientmanagement/repository/CommissionDAO.java
@@ -1,6 +1,9 @@
 package com.example.clientmanagement.repository;
 
 import com.example.clientmanagement.entity.Commission;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -12,67 +15,40 @@ import java.util.List;
 
 @Repository
 public class CommissionDAO {
-    private final DataSource dataSource;
 
-    private final SessionFactory sessionFactory;
-
-    public CommissionDAO(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.sessionFactory = new Configuration()
-                .addAnnotatedClass(com.example.clientmanagement.entity.Bill.class)
-                .addAnnotatedClass(com.example.clientmanagement.entity.Customer.class)
-                .addAnnotatedClass(com.example.clientmanagement.entity.Commission.class)
-                .configure()
-                .buildSessionFactory();
-    }
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<Commission> findAll() {
-        Session session = sessionFactory.openSession();
-        List<Commission> commissions = session.createQuery("from Commission", Commission.class).getResultList();
-        session.close();
-        return commissions;
+        return entityManager.createQuery("from Commission", Commission.class).getResultList();
     }
 
     public List<Commission> findAllByClient(Long customerId) {
-        Session session = sessionFactory.openSession();
-        List<Commission> commissions = session.createQuery("from Commission where customer.id = :id", Commission.class)
+        return entityManager.createQuery("from Commission where customer.id = :id", Commission.class)
                 .setParameter("id", customerId)
                 .getResultList();
-        session.close();
-        return commissions;
     }
 
     public Commission findCommissionById(Long id) {
-        Session session = sessionFactory.openSession();
-        Commission commission = session.find(Commission.class, id);
-        session.close();
-        return commission;
+        return entityManager.find(Commission.class, id);
     }
 
+    @Transactional
     public Commission create(Commission commission) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(commission);
-        transaction.commit();
-        session.close();
+        entityManager.persist(commission);
         return commission;
     }
 
+    @Transactional
     public Commission editCommission(Commission commission) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Commission newCommission = session.merge(commission);
-        transaction.commit();
-        session.close();
-        return newCommission;
+        return entityManager.merge(commission);
     }
 
+    @Transactional
     public void deleteCommission(Long id) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Commission commission = session.find(Commission.class, id);
-        session.remove(commission);
-        transaction.commit();
-        session.close();
+        Commission commission = findCommissionById(id);
+        if (commission != null) {
+            entityManager.remove(commission);
+        }
     }
 }
