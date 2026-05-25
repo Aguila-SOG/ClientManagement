68dd7d84e091c6d0efdfde3a5d613d40215c696c
Mattraii <matloprai@alu.edu.gva.es>
2026-05-25 18:50:32 +0200

    actualizacion controller de cuota con las rutas nuevas

--- ClientManagement/src/main/java/com/example/clientmanagement/controller/QuoteController.java
+++ ClientManagement/src/main/java/com/example/clientmanagement/controller/QuoteController.java
@@ -16,27 +16,27 @@ public class QuoteController {
         this.quoteService = quoteService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public List<Quote> findAll() {
         return quoteService.findAll();
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public Quote create(@RequestBody Quote quote) {
         return quoteService.create(quote);
     }
 
-    @GetMapping("/{year}")
+    @GetMapping("/search/{year}")
     public List<Quote> findQuote(@PathVariable int year) {
         return quoteService.findQuote(year);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/edit")
     public Quote editQuote(@RequestBody Quote quote) {
         return quoteService.editQuote(quote);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{year}/{quarterly}")
     public void deleteQuote(@PathVariable int year, @PathVariable int quarterly) {
         quoteService.deleteQuote(year, quarterly);
     }
