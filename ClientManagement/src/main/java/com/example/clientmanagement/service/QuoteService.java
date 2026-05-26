f31f8134527bfef3e7813f49b230ea52ecf79f0e
Gonzalo <gonrocmin@alu.edu.gva.es>
2026-05-26 13:21:07 +0200

    Implementados los calculos mensuales en QuoteService

--- ClientManagement/src/main/java/com/example/clientmanagement/service/QuoteService.java
+++ ClientManagement/src/main/java/com/example/clientmanagement/service/QuoteService.java
@@ -33,4 +33,31 @@ public class QuoteService {
     public void deleteQuote(int year, int quarterly) {
         quoteDAO.deleteQuote(year, quarterly);
     }
+
+    public double calcTotalFactured(int year, int month) {
+        List<Quote> yearlyQuotes = quoteDAO.findQuote(year);
+        double total = 0.0;
+
+        for (Quote quote : yearlyQuotes) {
+            if (quote.getDatePay() != null) {
+                int quoteMonth = quote.getDatePay().getMonth() + 1;
+                if (quoteMonth == month) {
+                    total += quote.getFacImport();
+                }
+            }
+        }
+        return total;
+    }
+
+    public double calcIrpfRetained(int year, int month) {
+        double totalFactured = calcTotalFactured(year, month);
+        return totalFactured * 0.20;
+    }
+
+    public double calcRealEarnings(int year, int month) {
+        double totalFactured = calcTotalFactured(year, month);
+        double irpfRetained = calcIrpfRetained(year, month);
+        return totalFactured - irpfRetained;
+    }
+
 }
